4287beeed2dbcc9801e422b19a8dd9eb0ff0fff7
Dmitrij <seo.wolf.d@gmail.com>
2020-01-29 18:06:56 +0100

    Spring Boot - Thymeleaf base template

---
+++ src/test/java/com/luv2code/springboot/thymleafdemo/ThymeLeafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
